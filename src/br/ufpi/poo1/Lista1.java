5d70cf07b4e2db90442aac5b48c64819ad5fb8fe
Alcemir R. Santos <alcemirsantos@gmail.com>
2018-08-23 16:54:08 -0300

    questao 3

--- src/br/ufpi/poo1/Lista1.java
+++ src/br/ufpi/poo1/Lista1.java
@@ -7,31 +7,46 @@ public class Lista1 {
 
 	static Scanner sc = new Scanner(System.in);
 	static DecimalFormat df = new DecimalFormat("#.##");
-	
-	public static void main(String[] args){
-		//questao1();
-		questao2();	
-		
+
+	public static void main(String[] args) {
+		// questao1();
+		// questao2();
+		questao3();
 	}
 
 	/**
-	 * 1. Fazer um algoritmo que receba com entrada o saldo de uma aplicação e 
+	 * 1. Fazer um algoritmo que receba com entrada o saldo de uma aplicação e
 	 * exiba como resultado o novo saldo após um reajuste de 10%.
 	 */
 	private static void questao1() {
 		System.out.print("Digite o saldo da aplicação: ");
 		double saldo = Double.valueOf(sc.nextLine());
-        System.out.print("O saldo após o reajuste é: " +df.format(saldo*1.1));
+		System.out
+				.print("O saldo após o reajuste é: " + df.format(saldo * 1.1));
 	}
-	
+
 	/**
-	 * 2. Faça um algoritmo que seja dado um valor para x como entrada para a 
+	 * 2. Faça um algoritmo que seja dado um valor para x como entrada para a
 	 * função f(x) = 3x2 + 2x + 7. O programa deve exibir o valor de f(x).
 	 */
 	private static void questao2() {
 		System.out.print("Digite o valor de \'x\': ");
 		double x = Double.valueOf(sc.nextLine());
-        System.out.print("O valor de f(x) é: " +df.format(3*x*x+2*x+7));
+		System.out.print("O valor de f(x) é: " + (3 * x * x + 2 * x + 7));
+	}
+
+	/**
+	 * 3. Faça um programa que receba um número e calcule seu antecessor e
+	 * sucessor.
+	 */
+	private static void questao3() {
+		System.out.print("Digite o valor de \'x\': ");
+		int x = Integer.valueOf(sc.nextLine());
+		System.out.println("Você digitou " + x + ".");
+		System.out.println("O sucecessor de " + x + " é " + (x+1) + ".");
+		System.out.println("O antecessor de " + x + " é " + (x-1) + ".");
+	}
+
 	}
 	}
 }
