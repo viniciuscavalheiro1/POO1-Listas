c7149ee6d70d677ac4006fe2770cc1608b4d403c
Alcemir R. Santos <alcemirsantos@gmail.com>
2018-08-23 16:47:23 -0300

    questao 2

--- src/br/ufpi/poo1/Lista1.java
+++ src/br/ufpi/poo1/Lista1.java
@@ -6,10 +6,11 @@ import java.util.Scanner;
 public class Lista1 {
 
 	static Scanner sc = new Scanner(System.in);
+	static DecimalFormat df = new DecimalFormat("#.##");
+	
 	public static void main(String[] args){
-		
-		questao1();
-
+		//questao1();
+		questao2();	
 		
 	}
 
@@ -20,8 +21,17 @@ public class Lista1 {
 	private static void questao1() {
 		System.out.print("Digite o saldo da aplicação: ");
 		double saldo = Double.valueOf(sc.nextLine());
-		DecimalFormat df = new DecimalFormat("#.##");
-        System.out.print("O saldo após o reajuste é:" +df.format(saldo*1.1));
+        System.out.print("O saldo após o reajuste é: " +df.format(saldo*1.1));
+	}
+	
+	/**
+	 * 2. Faça um algoritmo que seja dado um valor para x como entrada para a 
+	 * função f(x) = 3x2 + 2x + 7. O programa deve exibir o valor de f(x).
+	 */
+	private static void questao2() {
+		System.out.print("Digite o valor de \'x\': ");
+		double x = Double.valueOf(sc.nextLine());
+        System.out.print("O valor de f(x) é: " +df.format(3*x*x+2*x+7));
 	}
 	}
 }
