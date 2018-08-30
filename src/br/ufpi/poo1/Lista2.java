aef73287aa23a114afe57f7ba64f64d047db40d7
Alcemir R. Santos <alcemirsantos@gmail.com>
2018-08-30 16:59:41 -0300

    lista 2 questoes 1 e 2

--- src/br/ufpi/poo1/Lista2.java
+++ src/br/ufpi/poo1/Lista2.java
@@ -5,10 +5,10 @@ import br.ufpi.poo1.aux.Lampada;
 public class Lista2 {
 
 	public static void main(String[] args){
-		
+		questao1e2();
 	}
 	
-	static void questao1(){
+	static void questao1e2(){
 		Lampada l = new Lampada();
 		l.liga();
 		l.observa();
