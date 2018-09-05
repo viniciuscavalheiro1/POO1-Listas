9524a4b4db465498948bca9d6f8aee058af59df8
Alcemir R. Santos <alcemirsantos@gmail.com>
2018-09-05 16:32:49 -0300

    questao 2 lista 3

---
+++ src/br/ufpi/poo1/aux/Produto.java
@@ -0,0 +1,61 @@
+package br.ufpi.poo1.aux;
+
+import java.text.DecimalFormat;
+
+public class Produto {
+
+	private String nome;
+	private double precoCusto;
+	private double precoVenda;
+	private double margemLucro;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPrecoVenda() {
+		return precoVenda;
+	}
+
+	public void setPrecoVenda(double precoVenda) {
+		if (precoVenda < this.precoCusto){
+			
+			System.out
+					.println("O preço de venda não pode ser menor que o preço de"
+							+ " custo, que é: " + getPrecoCusto());
+			return;
+		}
+		this.precoVenda = precoVenda;
+	}
+
+	public double getMargemLucro() {
+		return margemLucro;
+	}
+
+	public void setMargemLucro(double margemLucro) {
+		this.margemLucro = margemLucro;
+	}
+
+	public double getPrecoCusto() {
+		return precoCusto;
+	}
+
+	public void setPrecoCusto(double valor) {
+		this.precoCusto = valor;
+	}
+	
+	public double calcularMargemLucro(){
+		return (this.precoVenda - this.precoCusto);
+	}
+	
+	public String getMargemLucroPorcentagem(){
+		DecimalFormat df = new DecimalFormat("#.##");
+		return df.format( calcularMargemLucro()/this.precoCusto )+"%";
+	}
+	
+
+}
--- src/br/ufpi/poo1/listas/Lista3.java
+++ src/br/ufpi/poo1/listas/Lista3.java
@@ -1,28 +1,50 @@
 package br.ufpi.poo1.listas;
 
+import java.util.Scanner;
+
 import br.ufpi.poo1.aux.Pessoa;
+import br.ufpi.poo1.aux.Produto;
 
 public class Lista3 {
 
-	public static void main(String[] args){
-		questao1();
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// questao1();
+		questao2();
 	}
 
 	private static void questao1() {
-		Pessoa p  = new Pessoa();
-		
+		Pessoa p = new Pessoa();
+
 		p.setNome("Alcemir");
 		p.setIdade(10);
 		p.setAltura(100.0);
 		p.setPeso(76.5);
-		
-		System.out.println("Nome: "+ p.getNome());
-		System.out.println("Idade: "+ p.getIdade());
-		System.out.println("Altura: " + p.getAltura()+"cm");
-		System.out.println("Massa Corporal: "+ p.getPeso()+"Kg");
 
-		System.out.println("IMC: "+ p.getIMC());
-		
+		System.out.println("Nome: " + p.getNome());
+		System.out.println("Idade: " + p.getIdade());
+		System.out.println("Altura: " + p.getAltura() + "cm");
+		System.out.println("Massa Corporal: " + p.getPeso() + "Kg");
+
+		System.out.println("IMC: " + p.getIMC());
+
 		p.imprime();
+	}
+
+	private static void questao2() {
+		Produto p = new Produto();
+		System.out.print("preço de custo: ");
+		p.setPrecoCusto(Double.valueOf(sc.next()));
+		do {
+			System.out.print("preço de venda: ");
+			p.setPrecoVenda(Double.valueOf(sc.next()));
+		} while (p.getPrecoVenda() <= 0.0);
+
+		System.out.println("Preço Custo (R$): " + p.getPrecoCusto());
+		System.out.println("Preço Venda (R$): " + p.getPrecoVenda());
+		System.out.println("Margem Lucro (R$): " + p.calcularMargemLucro());
+		System.out
+				.println("Margem Lucro (%): " + p.getMargemLucroPorcentagem());
 	}
 }
