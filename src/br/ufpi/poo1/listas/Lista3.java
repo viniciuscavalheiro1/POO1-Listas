036c9ca7de67db222ad1ac4232e7ae7e4eca5f9e
Vinicius Loiola <viniciuspbu@gmail.com>
2018-09-16 18:15:34 -0300

    Lista 2 e 3 atualizadas

--- src/br/ufpi/poo1/listas/Lista3.java
+++ src/br/ufpi/poo1/listas/Lista3.java
@@ -2,6 +2,7 @@ package br.ufpi.poo1.listas;
 
 import java.util.Scanner;
 
+import br.ufpi.poo1.aux.Conta_3;
 import br.ufpi.poo1.aux.Pessoa;
 import br.ufpi.poo1.aux.Produto;
 
@@ -11,7 +12,8 @@ public class Lista3 {
 
 	public static void main(String[] args) {
 		// questao1();
-		questao2();
+		//questao2();
+		questao3();
 	}
 
 	private static void questao1() {
@@ -44,7 +46,25 @@ public class Lista3 {
 		System.out.println("Preço Custo (R$): " + p.getPrecoCusto());
 		System.out.println("Preço Venda (R$): " + p.getPrecoVenda());
 		System.out.println("Margem Lucro (R$): " + p.calcularMargemLucro());
-		System.out
-				.println("Margem Lucro (%): " + p.getMargemLucroPorcentagem());
+		System.out.println("Margem Lucro (%): " + p.getMargemLucroPorcentagem());
 	}
+	
+	private static void questao3() {
+
+        Conta_3 conta1 = new Conta_3(1, "Joao", 100.00);
+
+        Conta_3 conta2 = new Conta_3(2, "Jessica", 4000.00);
+        conta1.saca(2500.00);
+        conta1.deposita(300.00);
+        conta2.saca(1000.00);
+        conta1.imprimeNome();
+        conta2.imprimeNome();
+        conta2.transfere(conta1, 2000.00);
+        conta1.transfere(conta2, 2500.00);
+        conta1.imprimeNome();
+        conta2.imprimeNome();        
+        conta1.setNome("Joao");
+        conta1.imprimeNome();
+
+    }
 }
